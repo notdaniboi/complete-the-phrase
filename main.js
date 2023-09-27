24c0dbb19ec389d6157464233ecb96ccbb84aab6
notdaniboi <144819170+notdaniboi@users.noreply.github.com>
2023-09-27 09:54:03 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("btn1").addEventListener("click", btn1Clicked);
+
+function btn1Clicked() {
+  alert("...the bush.");
+}
+
+document.getElementById("btn2").addEventListener("click", btn2Clicked);
+
+function btn2Clicked() {
+  alert("...leg.");
+}
+
+document.getElementById("btn3").addEventListener("click", btn3Clicked);
+
+function btn3Clicked() {
+  alert("...in disguise");
+}
